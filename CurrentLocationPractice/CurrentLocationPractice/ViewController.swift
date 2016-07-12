c8d295f35e240fe28e09f9b6fb72d6c50608386b
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-11 21:11:59 -0600

    Got the app to drop a pin where I wanted

--- CurrentLocationPractice/CurrentLocationPractice/ViewController.swift
+++ CurrentLocationPractice/CurrentLocationPractice/ViewController.swift
@@ -19,12 +19,17 @@ class ViewController: UIViewController,CLLocationManagerDelegate, MKMapViewDeleg
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.locationManagerThings()
+        
+   }
+    
+    func locationManagerThings() {
         locationManager = CLLocationManager()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest;
         locationManager.delegate = self;
         
         // user activated automatic authorization info mode
-        var status = CLLocationManager.authorizationStatus()
+        let status = CLLocationManager.authorizationStatus()
         if status == .NotDetermined || status == .Denied || status == .AuthorizedWhenInUse {
             // present an alert indicating location authorization required
             // and offer to take the user to Settings for the app via
@@ -40,8 +45,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate, MKMapViewDeleg
         mapView.showsUserLocation = true
         mapView.mapType = MKMapType(rawValue: 0)!
         mapView.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
-        
-   }
+    }
     
     func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation){
         
@@ -50,13 +54,61 @@ class ViewController: UIViewController,CLLocationManagerDelegate, MKMapViewDeleg
 
     @IBAction func buttonTapped(sender: UIBarButtonItem) {
         
-     let annotation = CustomPointAnnotation()   
+        self.addPin(40.439324, longitude: -111.815627, titleString: "Frost Home", subtitleString: "Sunset Hills Drive")
         
+        let latitude = 40.439324
         
+        let longitude = -111.815627
         
+        let location = CLLocationCoordinate2D(
+            latitude: latitude,
+            longitude: longitude
+        )
+        
+        self.centerMap(location)
+       
+    }
+    
+    func addPin(latitude: Double, longitude: Double, titleString: String, subtitleString: String) {
+        
+        // 2. Create a CLLocationCoordinate2D
+        
+        let location = CLLocationCoordinate2D(
+            latitude: latitude,
+            longitude: longitude
+        )
+        
+        // 3. Create an instance of MKPointAnnotation
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = location
+        annotation.title = titleString
+        annotation.subtitle = subtitleString
+        
+        // 4. Add to the annotation to the mapView
+        self.mapView.addAnnotation(annotation)
+    }
+    
+    func centerMap(centerCoordinate: CLLocationCoordinate2D) {
+        
+        // 1. Create an CoordinateSpan
+        let span = MKCoordinateSpan(
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1
+        )
+        
+        // 2. Create the Region
+        
+        let region = MKCoordinateRegion(
+            center: centerCoordinate,
+            span: span
+        )
+        
+        // 3. Add
+        self.mapView.setRegion(region, animated: true)
         
     }
-
 
 }
 
